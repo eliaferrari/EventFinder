d235424e780db64876f646f60caa676fe550e33f
Elia <39693711+eliaferrari@users.noreply.github.com>
2020-03-20 10:04:26 +0100

    Add files via upload

---
+++ Example/ol/olms.js
@@ -0,0 +1,6 @@
+import olms, {apply, applyBackground, applyStyle} from './index';
+import stylefunction from './stylefunction';
+
+Object.assign(olms, {apply, applyBackground, applyStyle, stylefunction});
+
+export default olms;
