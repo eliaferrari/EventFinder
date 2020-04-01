1a34d8cf2864cd22bbba4addd77ca1bb72a9c749
enegenti <enea.gentilini@gmail.com>
2020-04-01 16:04:21 +0200

    geoserver setup

--- geoserver/data_dir/www/Project/Leaflet.js
+++ geoserver/data_dir/www/Project/Leaflet.js
@@ -44,4 +44,37 @@ var wmsLayer= L.tileLayer.wms("http://localhost:8080/geoserver/gwc/service/wms",
     format: 'image/png',
     transparent: true
 });
-map.addLayer(wmsLayer);
+//map.addLayer(wmsLayer);
+
+        var camping = new L.tileLayer.wms("http://vps143339.ovh.net:8080/geoserver/camping/wms", {
+            layers: 'eventfinder:events',
+            format: 'image/png',
+            srs: "EPSG:3857",
+            transparent: true,
+            pointerCursor: true
+        }).addTo(map);
+        map.addEventListener('click', Identify);
+
+        function Identify(e) {
+            var BBOX = map.getBounds().toBBoxString();
+            var WIDTH = map.getSize().x;
+            var HEIGHT = map.getSize().y;
+            var X = map.layerPointToContainerPoint(e.layerPoint).x;
+            var Y = map.layerPointToContainerPoint(e.layerPoint).y;
+            var URL = wms_server + '&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&LAYERS=camping:camping&QUERY_LAYERS=camping:camping&BBOX=' + BBOX + '&FEATURE_COUNT=1&HEIGHT=' + HEIGHT + '&WIDTH=' + WIDTH + '&INFO_FORMAT=text%2Fhtml&SRS=EPSG%3A3857&X=' + X + '&Y=' + Y;
+            $.ajax({
+                url: URL,
+                datatype: "html",
+                type: "GET",
+                success: function(data) {
+                    var popup = new L.popup({
+                        maxWith: 300
+                    });
+                    popup.setContent(data);
+                    popup.setLatLng(e.latlng);
+                    map.openPopup(popup);
+                }
+            });
+        }
+
+//GEOSERVER WITH GEOJSO
