33b06286b74baa909a9bb5240fdcc02671a3d6f6
Elia Ferrari <elia.ferrari@students.fhnw>
2020-04-28 19:56:31 +0200

    Update form.js

--- geoserver/data_dir/www/Project/form.js
+++ geoserver/data_dir/www/Project/form.js
@@ -20,14 +20,17 @@ function showPosition(position) {
 }
 
 var owsrootUrl = 'http://localhost:8080/geoserver/eventfinder/ows';
-//Bbox solo quadrata quindi dal cerchio devo calcolare area equivalente
-//L=sqrt( r^2 * pi) --> L/2 in direzione x e y --> basso sinistra, alto a destra
-//Funcion BBOX request
+//L=sqrt( r^2 * pi) --> L/2 in direzione x e y --> basso sinistra, alto a destra (46.9627468;7.4497108)
+//BBOX
 var radius = document.getElementById("umkreis");
 var y1 = position.coords.longitude - (Math.sqrt(Number(radius)*Number(radius)*Math.PI)/2);
 var y2 = position.coords.longitude + (Math.sqrt(Number(radius)*Number(radius)*Math.PI)/2);
 var x1 = position.coords.latitude - (Math.sqrt(Number(radius)*Number(radius)*Math.PI)/2);
 var x2 = position.coords.latitude + (Math.sqrt(Number(radius)*Number(radius)*Math.PI)/2);
+//filter by event type
+var eventtype = document.getElementById("eventtype");
+//filter by date (future events)
+var date = document.getElementById("date");// formato da verificare (yyyy-mm-dd hh:mm:ss)
 
 var defaultParameters = {
     service : 'WFS',
@@ -35,39 +38,14 @@ var defaultParameters = {
     request : 'GetFeature',
     typeNames : 'eventfinder:events',
     srsName : 'EPSG:4326',
-    Bbox : 'x1,y1,x2,y2',//lon,lat?
+    Bbox : 'x1,y1,x2,y2',//lon,lat must be a number!!
+    cql_filter : 'catname=eventtype AND datum>date',// between (&cql_filter=datum BETWEEN '2020-08-01 00:00:00'AND'2020-08-15 00:00:00')
     outputFormat : 'text/javascript',
     format_options : 'callback:getJson'
 };
 
-
 //http://localhost:8080/geoserver/wfs?service=wfs&version=2.0&request=GetFeature&typeNames=eventfinder:events&cql_filter=catname='concert'
-//Funcion filter by event type
-var eventtype = document.getElementById("eventtype");
-var defaultParameters = {
-    service : 'WFS',
-    version : '2.0',
-    request : 'GetFeature',
-    typeNames : 'eventfinder:events',
-    cql_filter : 'catname=eventtype',
-    outputFormat : 'text/javascript',
-    format_options : 'callback:getJson',
-    srsName : 'EPSG:4326'
-};
-
 //http://localhost:8080/geoserver/wfs?service=wfs&version=2.0&request=GetFeature&typeNames=eventfinder:events&cql_filter=datum>'2020-08-30 00:00:00'
-//Funcion filter by date (just future events)
-var date = document.getElementById("date");// formato da verificare (yyyy-mm-dd hh-mm-ss)
-var defaultParameters = {
-    service : 'WFS',
-    version : '2.0',
-    request : 'GetFeature',
-    typeNames : 'eventfinder:events',// between (&cql_filter=datum BETWEEN '2020-08-01 00:00:00'AND'2020-08-15 00:00:00')
-    cql_filter : 'datum>date',//
-    outputFormat : 'text/javascript',
-    format_options : 'callback:getJson',
-    srsName : 'EPSG:4326'
-};
 
 //Funcion for the server request
 var parameters = L.Util.extend(defaultParameters);
@@ -78,16 +56,16 @@ var ajax = $.ajax({
     url : URL,
     dataType : 'jsonp',
     jsonpCallback : 'getJson',
-    success : function (response) {
+    success : function (response) {//in leaflet.js already defined
         WFSLayer = L.geoJson(response, {
-            style: function (feature) {
+            style: function (feature) {//in leaflet.js already defined
                 return {
                     stroke: false,
                     fillColor: 'FFFFFF',
                     fillOpacity: 0
                 };
             },
-            onEachFeature: function (feature, layer) {
+            onEachFeature: function (feature, layer) {//in leaflet.js already defined
                 popupOptions = {maxWidth: 300};
                 layer.bindPopup(feature.properties.name + "<br> " +
                 feature.properties.datum + "<br><br> " +
