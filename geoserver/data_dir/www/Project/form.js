2a4161a2555a3e38417ff47520807f8caca471db
enegenti <enea.gentilini@gmail.com>
2020-05-01 10:01:57 +0200

    big changes

--- geoserver/data_dir/www/Project/form.js
+++ geoserver/data_dir/www/Project/form.js
@@ -1,7 +1,17 @@
+var map = L.map('map').setView([46.9, 8.1], 8);
 
+var original =
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  maxZoom: 15,
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox.streets'
+}).addTo(map);
 
 //Geolocation
-var x = document.getElementById("location");
+var pos = 0
+var x = document.getElementById("myLocation");
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -15,34 +25,45 @@ function getLocation() {
 var test = "Insert CAP or city";
 document.getElementById("myLocation").value = test;
 function showPosition(position) {
+  pos = position;
   document.getElementById("myLocation").value = position.coords.latitude + ";"+ position.coords.longitude;
 
-}
 
+}
+function geolocation(){
+
+  var original =
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 15,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets'
+  }).addTo(map);
 var owsrootUrl = 'http://localhost:8080/geoserver/eventfinder/ows';
 //BBOX
-var radius = (Number(document.getElementById("umkreis"))/100);
-var y1 = position.coords.longitude - (Math.sqrt(radius*radius*Math.PI)/2);
-var y2 = position.coords.longitude + (Math.sqrt(radius*radius*Math.PI)/2);
-var x1 = position.coords.latitude - (Math.sqrt(radius*radius*Math.PI)/2);
-var x2 = position.coords.latitude + (Math.sqrt(radius*radius*Math.PI)/2);
+var radius = ((document.getElementById("umkreis").value)/100);
+var y1 = pos.coords.longitude - (Math.sqrt(radius*radius*Math.PI)/2);
+var y2 = pos.coords.longitude + (Math.sqrt(radius*radius*Math.PI)/2);
+var x1 = pos.coords.latitude - (Math.sqrt(radius*radius*Math.PI)/2);
+var x2 = pos.coords.latitude + (Math.sqrt(radius*radius*Math.PI)/2);
 var comma = ','
-var stringa = x1.toString().concat(comma,y1.toString(),comma,x2.toString(),comma,y2.toString());
+var stringa = y1.toString().concat(comma,x1.toString(),comma,y2.toString(),comma,x2.toString());
+console.log(stringa);
 //filter by event type
-var eventtype = document.getElementById("eventtype");
+var eventtype = document.getElementById("eventtype").value;
 //filter by date (future events)
-var eventdate = document.getElementById("eventdate");// formato da verificare, deve essere: (yyyy-mm-dd hh:mm:ss)
-var exampledate = '2020-08-15 00:00:00'
+var eventdate = document.getElementById("eventdate").value.concat(" 00:00:01");// formato da verificare, deve essere: (yyyy-mm-dd hh:mm:ss)
+
 var defaultParameters = {
     service : 'WFS',
     version : '2.0',
     request : 'GetFeature',
     typeNames : 'eventfinder:events',
     srsName : 'EPSG:4326',
-    bbox : stringa,
-    cql_filter : "catname="+"'"+eventtype+"'"+" AND datum>"+"'"+eventdate+"'",// between (&cql_filter=datum BETWEEN '2020-08-01 00:00:00'AND'2020-08-15 00:00:00')
     outputFormat : 'text/javascript',
-    format_options : 'callback:getJson'
+    format_options : 'callback:getJson',
+    cql_filter : "catname="+"'"+eventtype+"'"+"AND datum>'"+eventdate+"'",// between (&cql_filter=datum BETWEEN '2020-08-01 00:00:00'AND'2020-08-15 00:00:00')
 };
 
 //http://localhost:8080/geoserver/eventfinder/ows?service=WFS&version=2.0&request=GetFeature&typeName=eventfinder:events&outputFormat=text/javascript&cql_filter=catname=%27sport%27
@@ -52,7 +73,7 @@ var defaultParameters = {
 //Funcion for the server request
 var parameters = L.Util.extend(defaultParameters);
 var URL = owsrootUrl + L.Util.getParamString(parameters);
-
+console.log(URL)
 var WFSLayer = null;
 var ajax = $.ajax({
     url : URL,
@@ -66,6 +87,7 @@ var ajax = $.ajax({
                     fillColor: 'FFFFFF',
                     fillOpacity: 0
                 };
+
             },
             onEachFeature: function (feature, layer) {//in leaflet.js already defined
                 popupOptions = {maxWidth: 300};
@@ -75,14 +97,27 @@ var ajax = $.ajax({
                 feature.properties.website  + "<br><br> " +
                 feature.properties.beschreibung,popupOptions);
             }
+
         }).addTo(map);
+        var zoom;
+        if ((document.getElementById("umkreis").value)==10) {zoom =12;}
+        else if ((document.getElementById("umkreis").value)==20) {zoom =11;}
+        else if ((document.getElementById("umkreis").value)==50) {zoom =9.5;}
+        else if ((document.getElementById("umkreis").value)==100) {zoom =8.5;}
+        else { zoom = 10}
+
+
+      map.flyTo([pos.coords.latitude, pos.coords.longitude], zoom);
     }
-});
+});}
 
 //Function Form
-function submit() {
-  document.getElementById("form1").submit();
-}
-function reset() {
-  document.getElementById("form1").reset()
-}
+function confirm() {
+  map.eachLayer(function (layer) {
+      map.removeLayer(layer);
+  });
+  geolocation();
+};
+function clean() {
+  document.getElementById("form1").reset();
+};
